5adaea26085c529e581f0e6cbf4a271c72b4d6d0
Canberk <canberkkaya06@gmail.com>
2022-05-30 14:35:10 +0300

    react router dom v6 implementation

---
+++ src/Pages/CreatePage/CreatePage.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export default function CreatePage() {
+  return (
+    <div>
+      <h1>Create</h1>
+      <Link to='/'>Back</Link>
+    </div>
+  )
+}
---
+++ src/Pages/MainPage/MainPage.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Main</h1>
+      <Link to='/create'>Create</Link>
+    </div>
+  )
+}
